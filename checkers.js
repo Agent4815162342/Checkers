c5960873bd5d2c1b5b7ae8f9a5ece9ec52da7b76
Agent4815162342 <inkognito.37@inbox.ru>
2019-06-13 15:27:01 +0300

    Final draw

--- checkers.js
+++ checkers.js
@@ -1,7 +1,7 @@
 class Checkers {
     constructor(options) {
         this.checkers = options.checkers;
-
+        this.team = 'w';
         this.draw();
     }
     draw() {
@@ -13,6 +13,15 @@ class Checkers {
                 elem.setAttribute('data-x', j);
                 elem.setAttribute('data-y', i);
                 if (counter%2 == 0) elem.classList.add('black_field');
+                if (i < 3 && elem.classList.contains('black_field')) {
+                    let checker = document.createElement('div');
+                    checker.classList.add('w');
+                    elem.appendChild(checker);
+                } else if (i > 4 && elem.classList.contains('black_field')) {
+                    let checker = document.createElement('div');
+                    checker.classList.add('b');
+                    elem.appendChild(checker);
+                }
                 this.checkers.appendChild(elem);
                 counter++;
             }
