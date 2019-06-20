28782a8bba5c85e639e3b4f055193ace7828bd0e
Agent4815162342 <inkognito.37@inbox.ru>
2019-06-20 15:18:42 +0300

    It's alive!

--- checkers.js
+++ checkers.js
@@ -14,7 +14,7 @@ class Checkers {
                 elem.classList.add('field');
                 elem.setAttribute('data-x', j);
                 elem.setAttribute('data-y', i);
-                if (counter%2 == 0) {
+                if (counter % 2 == 0) {
                     elem.classList.add('black_field');
                     let check = document.createElement('div');
                     if (i < 3) check.classList.add('w');
@@ -28,19 +28,19 @@ class Checkers {
             counter++;
         }
     }
-    step(team) {                        // Метод, задающий обработчик для нажатия на шашку                                                       
-        let y = 0;                                                     
-        this.checkers.onclick =  (e) =>  {                             
-            this.clear('active_field');                                                               
+    step(team) {                        // Метод, задающий обработчик для нажатия на шашку                                                                                                            
+        this.checkers.onclick = (e) => {
+            console.log(1);
+            this.clear('active_field');
             this.currentCheck = e.target.closest(`.${team}`)
             if (this.currentCheck) {
                 this.addActiveField(this.currentCheck);
-                this.confirm();
             }
         }
     }
     confirm() {                         // Метод, перемещающий шашку на возможный ход, в противном случае подсвечивает ходы для другой шашки
         this.checkers.onclick = (e) => {
+            console.log(2);
             if (e.target.closest('.active_field')) {
                 e.target.appendChild(this.currentCheck);
                 this.clear('active_field');
@@ -48,9 +48,9 @@ class Checkers {
                 this.toggleTeam();
                 this.step(this.team);
             } else if (e.target.closest(`.${this.team}`)) {
-                    this.clear('active_field');
-                    this.currentCheck = e.target.closest(`.${this.team}`);
-                    this.addActiveField(this.currentCheck);
+                this.clear('active_field');
+                this.currentCheck = e.target.closest(`.${this.team}`);
+                this.addActiveField(this.currentCheck);
             } else {
                 return;
             }
@@ -70,16 +70,14 @@ class Checkers {
         }
     }
     addActiveField(current) {           // Метод, подссвечивающий активные ходы
-        this.clear('eat'); 
-        this.coordX = +current.closest('.field').dataset.x;
-        this.coordY = +current.closest('.field').dataset.y;
-
-        for (let i = -1; i< 2; i+=2) {
-            for (let j = -1; j < 2; j+=2) {
-                if (this.coordX+i < 0 || this.coordX+i >7 || this.coordY+j < 0 || this.coordY+j > 7) continue;
-                let potentialField = document.querySelector(`div[data-y="${this.coordY+j}"][data-x="${this.coordX+i}"]`);
+        this.coordX = +current.parentNode.dataset.x;
+        this.coordY = +current.parentNode.dataset.y;
+        for (let i = -1; i < 2; i += 2) {
+            for (let j = -1; j < 2; j += 2) {
+                if (this.coordX + i < 0 || this.coordX + i > 7 || this.coordY + j < 0 || this.coordY + j > 7) continue;
+                let potentialField = document.querySelector(`div[data-y="${this.coordY + j}"][data-x="${this.coordX + i}"]`);
                 if (potentialField.children.length > 0 && !potentialField.children[0].classList.contains(this.team)) {
-                    this.eat(potentialField);
+                    this.eat.call(this, potentialField);
                 } else if (this.team == 'w' && j == -1) {
                     continue;
                 } else if (this.team == 'b' && j == 1) {
@@ -87,39 +85,43 @@ class Checkers {
                 } else {
                     if (!document.querySelector('.eat') && potentialField.children.length == 0) {
                         potentialField.classList.add('active_field');
-                        this.confirm(); 
+                        this.confirm.call(this);
                     }
                 }
             }
         }
     }
     eat(field) {
-        let currentField = this.currentCheck.parentNode;
-        let shiftX = +field.dataset.x - +currentField.dataset.x;
-        let shiftY = +field.dataset.y - +currentField.dataset.y;
+        this.currentField = this.currentCheck.parentNode;
+        let shiftX = +field.dataset.x - +this.currentField.dataset.x;
+        let shiftY = +field.dataset.y - +this.currentField.dataset.y;
         if (+field.dataset.y + shiftY < 0 || +field.dataset.y + shiftY > 7 || +field.dataset.x + shiftX < 0 || +field.dataset.x + shiftX > 7) return;
         let eatField = document.querySelector(`div[data-y="${+field.dataset.y + shiftY}"][data-x="${+field.dataset.x + shiftX}"]`);
         if (!eatField.children.length) {
             this.clear('active_field');
             eatField.classList.add('eat');
-            eatField.onclick = this.eatConfirm.bind(this);
+            eatField.onclick = this.eatConfirm.bind(this, eatField);
 
         }
     }
     eatConfirm(field) {
-            let currentCheck = this.currentCheck.parentNode;
-            let target = document.querySelector('.eat');
-            console.dir(target);
-            let removedField = document.querySelector(`div[data-y="${(+target.dataset.y + +currentCheck.dataset.y)/2}"][data-x="${(+target.dataset.x + +currentCheck.dataset.x)/2}"]`);
-            if (removedField.children.length > 0) {
-                removedField.removeChild(removedField.children[0]);
-                target.appendChild(this.currentCheck);
-                this.currentCheck = target.children[0];
-                this.clear('eat');
+        console.log(3);
+        this.clear('eat');
+        this.checkers.onclick = null;
+        let removedField = document.querySelector(`div[data-y="${(+field.dataset.y + +this.currentField.dataset.y) / 2}"][data-x="${(+field.dataset.x + +this.currentField.dataset.x) / 2}"]`);
+        if (removedField != null && removedField.children.length > 0) {
+            removedField.removeChild(removedField.children[0]);
+            field.appendChild(this.currentCheck);
+            this.addActiveField(this.currentCheck);
+            this.currentCheck.onclick = null;
+            let search = document.querySelectorAll('.eat');
+            if (search.length == 0) {
                 this.toggleTeam();
                 this.step(this.team);
             }
         }
+        field.onclick = null;
+    }
 }
 
 const myCheckers = new Checkers({
